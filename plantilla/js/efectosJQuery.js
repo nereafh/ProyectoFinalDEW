1d4324e480e8f5e95aab08fd1c57f4c14bac98ec
Nerea <nereafernandezh@gmail.com>
2026-02-02 12:52:21 +0000

    Ultimos cambios

--- plantilla/js/efectosJQuery.js
+++ plantilla/js/efectosJQuery.js
@@ -1,25 +1,23 @@
 $(document).ready(function() {
-    
-    //-----------------EFECTO LOGO-------------------
+    // Usamos delegación de eventos para que funcionen aunque Vue cambie el DOM
     $(window).scroll(function() {
-        if ($(this).scrollTop() > 50) {
-            $('#logo').stop().animate({ width: '60px' }, 200);
-        } else {
-            $('#logo').stop().animate({ width: '85px' }, 200);
+        var logo = $('#logo');
+        if (logo.length) { // Solo si el logo existe
+            if ($(this).scrollTop() > 50) {
+                logo.stop().animate({ width: '60px' }, 200);
+            } else {
+                logo.stop().animate({ width: '85px' }, 200);
+            }
         }
     });
 
-    //---------------HOVER PRODUCTOS (CodePen 3)----------------------
     $(document).on('mouseenter', '.prod-item', function() {
-        $(this).stop().animate({ opacity: 0.7 }, 200);
+        $(this).stop().fadeTo(200, 0.7);
     }).on('mouseleave', '.prod-item', function() {
-        $(this).stop().animate({ opacity: 1 }, 200);
+        $(this).stop().fadeTo(200, 1);
     });
 
-    //------------------EFECTO AL CONFIRMAR COMPRA (CodePen 4: SlideUp)----------------
-    // Cuando le das a "Confirmar Pedido", la tabla del carrito desaparece deslizándose
     $(document).on('click', '.btn-success', function() {
         $('table').slideUp(1000);
     });
-
 });
